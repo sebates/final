58a28cca9453baf122df381e56672c9829fe224a
sebates <smbates1@icloud.com>
2024-06-09 16:36:56 -0400

    test

--- index.js
+++ index.js
@@ -7,22 +7,27 @@ const database = [
 
 function search(){
 
-
-  var filter = document.getElementById("filter").value;
-  document.getElementById("test").innerText = filter;
+  document.getElementById("test").innerText = document.getElementById("filter").value;
 
   var results = []
-  for(var i=0;i<database.length;i++){
-    if(database[i].level.toLowerCase().startsWith(document.getElementById("input").value.toLowerCase())) {
-      results.push(database[i].levelId);
+
+  if(document.getElementById("filter").value === "level"){
+    for(var i=0;i<database.length;i++){
+      if(database[i].level.toLowerCase().startsWith(document.getElementById("input").value.toLowerCase())) {
+        results.push(database[i].levelId);
+      }
     }
   }
 
+  if(document.getElementById("filter").value === "levelId"){
+    for(var i=0;i<database.length;i++){
+      if(database[i].levelId.startsWith(document.getElementById("input").value)) {
+        results.push(database[i].levelId);
+      }
+    }
+  }
 
-
-
-
-  document.getElementById("results").innerText = results.length+" levels found";
+  document.getElementById("results").innerText = results.length + " results";
   document.getElementById("levelName0").innerText = database.find(x => x.levelId === results[0]).level;
   document.getElementById("levelId0").innerText = "ID: "+results[0];
 };
